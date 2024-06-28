7f7efa739191469caa912d57de4c3c4c2c163719
Karthikeyan Chinnakonda <karthikeyan@hasura.io>
2024-06-28 16:45:24 +0530

    Fix mutation tests (#140)
    <!-- The PR description should answer 2 (maybe 3) important questions:
    -->
    
    ### What
    
    <!-- What is this PR trying to accomplish (and why, if it's not
    obvious)? -->
    This PR separates the testing configuration with the sample
    configuration to enable mutation tests to run without any dependency on
    the sample configuration.
    
    ### How
    
    <!-- How is it trying to accomplish it (what are the implementation
    steps)? -->

---
+++ static/introspect_stored_procedures.sql
@@ -0,0 +1,23 @@
+SELECT ISNULL(
+  (SELECT
+     s.name AS [schema],
+     p.name AS [name],
+     (
+       SELECT
+         pr.parameter_id,
+         /* Remove the '@' prefix from every argument name */
+         SUBSTRING(pr.name, 2, LEN(pr.name) - 1) as name,
+         t.name AS [type],
+         pr.max_length,
+         pr.is_output,
+         pr.is_nullable
+         FROM sys.parameters pr
+              INNER JOIN sys.types t ON pr.user_type_id = t.user_type_id
+        WHERE pr.object_id = p.object_id
+        ORDER BY pr.parameter_id
+        FOR JSON PATH
+     ) AS arguments
+     FROM sys.procedures p
+     INNER JOIN sys.schemas s ON p.schema_id = s.schema_id
+     FOR JSON PATH
+  ), '[]')
