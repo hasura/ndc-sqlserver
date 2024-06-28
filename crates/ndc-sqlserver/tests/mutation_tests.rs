483a5fad1ca2fa2cad65eba92a7fc1cb74c1cdc0
Karthikeyan C <karthikeyan@hasura.io>
2024-06-28 13:29:54 +0530

    add more tests

--- crates/ndc-sqlserver/tests/mutation_tests.rs
+++ crates/ndc-sqlserver/tests/mutation_tests.rs
@@ -6,10 +6,7 @@ mod native_mutations {
 
     use super::common::fresh_deployments::FreshDeployment;
 
-    use serial_test::serial;
-
     #[tokio::test(flavor = "multi_thread")]
-    #[serial]
     async fn native_mutation_insert_artist_and_return_id() {
         let original_db_config = MSSQLDatabaseConfig::original_db_config();
         let fresh_deployment = FreshDeployment::create(original_db_config, "static/tests", vec![])
@@ -26,7 +23,6 @@ mod native_mutations {
     }
 
     #[tokio::test(flavor = "multi_thread")]
-    #[serial]
     /// Native mutation that selects a relationship.
     async fn native_mutation_insert_artist_and_return_artist() {
         let original_db_config = MSSQLDatabaseConfig::original_db_config();
@@ -52,10 +48,8 @@ mod native_mutations {
         use super::*;
 
         use hyper::StatusCode;
-        use serial_test::serial;
 
         #[tokio::test(flavor = "multi_thread")]
-        #[serial]
         async fn test_atomicity_native_mutations() {
             let original_db_config = MSSQLDatabaseConfig::original_db_config();
             let fresh_deployment =
@@ -95,10 +89,8 @@ mod stored_procedures {
     use super::common::fresh_deployments::FreshDeployment;
 
     use hyper::StatusCode;
-    use serial_test::serial;
 
     #[tokio::test(flavor = "multi_thread")]
-    #[serial]
     async fn basic_stored_procedure_execution() {
         let original_db_config = MSSQLDatabaseConfig::original_db_config();
 
@@ -123,7 +115,6 @@ mod stored_procedures {
     }
 
     #[tokio::test(flavor = "multi_thread")]
-    #[serial]
     async fn execute_stored_procedure_without_providing_arguments() {
         let original_db_config = MSSQLDatabaseConfig::original_db_config();
 
@@ -143,6 +134,31 @@ mod stored_procedures {
             "mutations/stored_procedures/get_customer_details_without_arguments",
             fresh_deployment.connection_uri.clone(),
             StatusCode::BAD_REQUEST,
+        )
+        .await;
+
+        insta::assert_json_snapshot!(result);
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn execute_stored_procedure_with_relationships() {
+        let original_db_config = MSSQLDatabaseConfig::original_db_config();
+
+        let stored_procs_setup_file_path = get_path_from_project_root(
+            "static/tests/stored_procedures_sql/get_customer_details.sql",
+        );
+
+        let fresh_deployment = FreshDeployment::create(
+            original_db_config,
+            "static/tests",
+            vec![stored_procs_setup_file_path],
+        )
+        .await
+        .unwrap();
+
+        let result = run_mutation(
+            "mutations/stored_procedures/get_customer_details_with_invoices",
+            fresh_deployment.connection_uri.clone(),
         )
         .await;
 
